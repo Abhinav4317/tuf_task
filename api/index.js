b70a043bb34846747facd7770ead5c2df90a20a7
Abhinav4317 <abhinavgangwar17@gmail.com>
2024-08-12 20:03:47 +0530

    second commit

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://tuf-task-sage.vercel.app",
   })
 );
 const PORT = process.env.PORT || 4000;
